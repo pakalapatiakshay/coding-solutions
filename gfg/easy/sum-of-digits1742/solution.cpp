f7f0a61e867a82bcf666c74a2b9cab8ebf5a8066
Akshay <pakalapatiakshay44@gmail.com>
2026-08-18 17:33:52 +0530

    ✅ Sum Of Digits | Easy | C++ | Runtime: N/A | Memory: N/A | 2026-08-18

---
+++ gfg/easy/sum-of-digits1742/solution.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+class Solution {
+  public:
+  int sumOfDigits(int n) {
+      int sum = 0;
+      while (n != 0) {
+
+        
+          int last = n % 10;
+
+          
+          sum += last;
+
+         
+          n /= 10;
+      }
+      return sum;
+  }};
