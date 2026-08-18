cf80ddbb2069c98967f6577f7fabef4cf9da0f7c
Akshay <pakalapatiakshay44@gmail.com>
2026-08-18 15:48:56 +0530

    ✅ LCPPS30B | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-18

---
+++ codechef/medium/LCPPS30B/solution.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  float temperature = 25.5;
+  int kelvin ;
+  cout<<"Celsius-"<< temperature<< endl;
+  
+  cout<< "Kelvin-"<< temperature+273;
+}
