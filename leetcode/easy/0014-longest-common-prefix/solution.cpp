2f8c68c0355114c79504f47e84f63659bb9bc570
Akshay <pakalapatiakshay44@gmail.com>
2026-08-21 19:01:04 +0530

    ✅ Longest Common Prefix | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 11.8 MB | 2026-08-21

--- leetcode/easy/0014-longest-common-prefix/solution.cpp
+++ leetcode/easy/0014-longest-common-prefix/solution.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string prefix = strs[0];
-        for (int i = 1; i < strs.size(); i++) {
-            int j = 0;
-                while (j < prefix.size() &&
-                   j < strs[i].size() &&
-                   prefix[j] == strs[i][j]) {
-                j++;}
-             prefix = prefix.substr(0, j);
-                if (prefix.empty()) {
-                return "";
+        string prefix=strs[0];
+        for(int i=1 ; i<strs.size() ; i++){
+            int j=0;
+            int k=0;
+            while(j<prefix.size() && k<strs[i].size()){
+                if(prefix[j]!=strs[i][k])
+                    break;
+                j++;
+                k++;
             }
+            prefix=prefix.substr(0,k);
         }
         return prefix;
     }
